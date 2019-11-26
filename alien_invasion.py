9a18c2bbee69121bb87214b4773a2437b7f8be49
Marcos Douglas <marcosdouglasclima@gmail.com>
2019-11-26 08:43:03 -0300

    apagando projéteis antigos

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,12 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        # Livra-se dos projéteis que desapareceram
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
