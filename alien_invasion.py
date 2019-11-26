9991d46c81303985cf759841fc5b64d694fa7e83
Marcos Douglas <marcosdouglasclima@gmail.com>
2019-11-25 22:08:53 -0300

    foi criado um grupo para armazenar os projéteis e foi feito uma breve alteração na função 'check_keydown_events()' para que o disparo ocorra ao pressionar a tecla espaço

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -13,11 +14,14 @@ def run_game():
 
     # Cria uma espaçonave
     ship = Ship(ai_settings, screen)
+    # Cria um grupo no qual serão armazenados os projéteis
+    bullets = Group()
 
     # Inicia o laço principal do jogo
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings():
 
         
         # Configurações dos projéteis
-        Self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
